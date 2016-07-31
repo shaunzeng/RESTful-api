6f5161ba02c7a2466a005366b47d7341ea3d4e88
Shaun <zengxiangxiang@gmail.com>
2016-07-31 16:57:30 -0400

    first commit

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var indexHandler = require('../controllers/indexHandler');
+var mSaveHandler = require('../controllers/mSaveHandler');
+var mGetHandler = require('../controllers/mGetHandler');
+var mPutHandler = require('../controllers/mPutHandler');
+var mPatchHandler = require('../controllers/mPatchHandler');
+var mDeleteHandler = require('../controllers/mDeleteHandler');
+var getStatsHandler = require('../controllers/getStatsHandler');
+
+
+router.get('/',indexHandler);
+router.post('/measurements', mSaveHandler);
+router.get('/measurements:timestamp', mGetHandler);
+router.put('/measurements/:timestamp',mPutHandler);
+router.patch('/measurements/:timestamp', mPatchHandler);
+router.delete('/measurements/:timestamp', mDeleteHandler);
+router.get('/stats', getStatsHandler);
+
+module.exports = router;
